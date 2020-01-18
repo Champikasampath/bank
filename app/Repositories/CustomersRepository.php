2318b9834714c0dee0a6471f7293956c5caa8bdd
Champika <sampathx123@gmail.com>
2020-01-18 23:28:01 +0530

    repos

---
+++ app/Repositories/CustomersRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: champika
+ * Date: 1/18/20
+ * Time: 11:22 PM
+ */
+
+namespace App\Repositories;
+
+
+class CustomersRepository
+{
+
+}
---
+++ app/Repositories/TransactionsRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: champika
+ * Date: 1/18/20
+ * Time: 11:21 PM
+ */
+
+namespace App\Repositories;
+
+
+class TransactionsRepository
+{
+
+}
